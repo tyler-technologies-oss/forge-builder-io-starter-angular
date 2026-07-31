215b06a1d29e3066b7c4c94d4336e1ae5389cae9
nickonometry <nickonometry@gmail.com>
2026-07-31 09:50:40 -0400

    feat: enhance dashboard layout with structured cards and inline message

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ForgeCardModule } from '@tylertech/forge-angular';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ForgeIconModule, ForgeInlineMessageModule } from '@tylertech/forge-angular';
+import { IconRegistry } from '@tylertech/forge';
+import '@tylertech/forge-extended/structured-card';
+import { tylIconInfo } from '@tylertech/tyler-icons';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ForgeCardModule],
+  imports: [ForgeIconModule, ForgeInlineMessageModule],
   templateUrl: './dashboard.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  static {
+    IconRegistry.define([tylIconInfo]);
+  }
+}
